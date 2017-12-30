ee0b6f43b214ea73f49aa7513020a0969314c012
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-30 15:12:15 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,10 +10,10 @@ end
 
 
 def current_player(board)
-  if turn_count(board) % 2 != 0
-      return "O"
-  else
+  if turn_count(board) % 2
     return "X"
+  elsif turn_count(board) % 2 != 0
+    return "O" 
 
   end
 end
