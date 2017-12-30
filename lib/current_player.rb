3d225d8c14078b40122cdfcacff57fdad3a25e6e
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-30 15:09:20 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,7 +12,7 @@ end
 def current_player(board)
   if turn_count(board) % 2
       return "X"
-  elsif turn_count(board) / 2 > 0 || turn_count(board) / 2 < 0
+  elsif !turn_count(board) % 2
       return "O"
   end
 end
