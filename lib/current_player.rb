5429250fec22d831082fc99fa94714ff3ba4d1b4
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-30 14:45:27 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,9 @@
 def turn_count(board)
   count = 0
   board.each do |turn|
-    if board[turn] == "X" || board[turn] == "O"
+    if board[turn] == "X"
+      count+= 1
+    elsif board[turn] == "O"
       count+= 1
     end
   end
